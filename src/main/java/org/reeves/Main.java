496478015a6b3b49315b8f2330a01d6370047e3c
SEVEER <SEVEER221@outlook.com>
2025-12-02 22:10:32 -0500

    yucyucucyuv

---
+++ src/main/java/org/reeves/Main.java
@@ -0,0 +1,18 @@
+package org.reeves;
+
+public class Main {
+    static void main() {
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            IO.println("i = " + i);
+        }
+        public static int dancing(int damn) {
+            return 1;
+        }
+
+        void testsoms() {
+            int idk = 1;
+        }
+    }
+}
