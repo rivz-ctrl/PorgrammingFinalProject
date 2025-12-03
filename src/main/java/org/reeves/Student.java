948d73f379fb8d532bc28ef555c2ab7fe95ff775
SEVEER <SEVEER221@outlook.com>
2025-12-02 23:59:26 -0500

    created registerCourses method and its documentation

--- src/main/java/org/reeves/Student.java
+++ src/main/java/org/reeves/Student.java
@@ -14,4 +14,16 @@ public class Student {
     public enum Gender {
         MALE, FEMALE;
     }
+
+    /**
+     * registers a course
+     * 1) adds the course to the student's registeredCourses lsit
+     * 2) adds the student to the course<s registeredStudents list
+     * 3) appends a null for the scores of each assignment of the course
+     * if the course is already registered, directly returns false without adding anything
+     * @param course the course to be registered
+     * @return whether the student can be registered
+     */
+    public boolean registerCourses(Course course) {
+    }
 }
