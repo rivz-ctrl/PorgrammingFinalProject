206303f1a7fe23b2537ac17a45bb85546d040a59
SEVEER <SEVEER221@outlook.com>
2025-12-02 23:10:43 -0500

    used lombok to add ToString, equals, getter, setter

--- src/main/java/org/reeves/Address.java
+++ src/main/java/org/reeves/Address.java
@@ -1,5 +1,14 @@
 package org.reeves;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@EqualsAndHashCode
+@Getter
+@Setter
 public class Address {
     int streetNo;
     String street;
