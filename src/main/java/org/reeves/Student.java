e48a418b55c279f39b9899695edc80c46ee7682b
SEVEER <SEVEER221@outlook.com>
2025-12-02 23:44:25 -0500

    wrote the fields of the Student class

--- src/main/java/org/reeves/Student.java
+++ src/main/java/org/reeves/Student.java
@@ -1,4 +1,13 @@
 package org.reeves;
 
+import java.util.ArrayList;
+
 public class Student {
+    private String studentId;
+    private String studentName;
+    private Gender gender;
+    private Address address;
+    private Department department;
+    private ArrayList<Course> registeredCourses;
+    private static int nextId;
 }
