89055e81bfc80b893b5a438a827d54ff31b3898f
SEVEER <SEVEER221@outlook.com>
2025-12-02 23:17:03 -0500

    created isDepartmentNameValid method

--- src/main/java/org/reeves/Department.java
+++ src/main/java/org/reeves/Department.java
@@ -1,4 +1,16 @@
 package org.reeves;
 
 public class Department {
+    String departmentId;
+    String departmentName;
+    static int nextId;
+
+    /**
+     * checks if a department name is valid or not (should only contain letters or space)
+     * @param departmentName department nam to be verified
+     * @return whether the department name is valid
+     */
+    static boolean isDepartmentNameValid(String departmentName) {
+        return true;
+    }
 }
