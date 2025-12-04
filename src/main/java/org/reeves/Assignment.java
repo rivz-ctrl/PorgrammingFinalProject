7bf30aa29dd843ba58bf3f30c7ac1639eec9536e
SEVEER <SEVEER221@outlook.com>
2025-12-03 23:15:15 -0500

    added field to class and created the calcAssignment method and its's documentation

--- src/main/java/org/reeves/Assignment.java
+++ src/main/java/org/reeves/Assignment.java
@@ -1,4 +1,18 @@
 package org.reeves;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Assignment {
+    private String assignmentId;
+    private String assignmentName;
+    private double weight;
+    private ArrayList<Integer> scores;
+    private static int nextId = 1;
+
+    /**
+     * calculates the average for the assignment
+     */
+    public void calcAssignment() {
+    }
 }
