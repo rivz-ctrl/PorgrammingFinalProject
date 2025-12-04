28c60a083e11905ef06861ad88e112c1cc50f10a
SEVEER <SEVEER221@outlook.com>
2025-12-04 10:55:52 -0500

    writing down to Teacher's String and toSimplifiedString for Course class

--- src/main/java/org/reeves/Course.java
+++ src/main/java/org/reeves/Course.java
@@ -1,5 +1,23 @@
 package org.reeves;
 
 public class Course {
+    public String toSimplifiedStringg() {
+        "}";
+    }
 
+    @Override
+    public String toString {
+        String message = "Student{"
+        "name=" + name + '\'' +
+                ", course=";
+        for (Student student : students) {
+            message += student.toSimplifiedStringg();
+        }
+
+        return message;
+    }
+
+    public list<Student> getStudents() {
+
+    }
 }
