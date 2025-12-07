2f05a65edd80399fc3f15b76c4ac14f87b85d6c5
SEVEER <SEVEER221@outlook.com>
2025-12-07 18:54:48 -0500

    completed toTitleCase method

--- src/main/java/org/reeves/util/Util.java
+++ src/main/java/org/reeves/util/Util.java
@@ -7,6 +7,27 @@ public class Util {
      * @return
      */
     public static String toTitleCase(String str) {
-        return ;
+
+        String[] words = str.split(" ");
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            if(word.isEmpty()) {
+                continue;
+            }
+
+            String firstPart = word.substring(0, 1).toUpperCase();
+            String rest = "";
+            if (word.length() > 1) {
+                rest = word.substring(1).toLowerCase();
+            }
+            if (!result.isEmpty()) {
+                result.append(" ");
+            }
+
+            result.append(firstPart).append(rest);
+        }
+        return result.toString();
     }
 }
