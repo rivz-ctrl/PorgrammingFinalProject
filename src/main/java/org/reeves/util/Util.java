4a42d446041d667125fbf6746fc34c6dca0c6b09
SEVEER <SEVEER221@outlook.com>
2025-12-07 16:59:08 -0500

    created util package, Util class, toTitleCase method

---
+++ src/main/java/org/reeves/util/Util.java
@@ -0,0 +1,12 @@
+package org.reeves.util;
+
+public class Util {
+    /**
+     * converts each word in a string to title case
+     * @param str
+     * @return
+     */
+    public static String toTitleCase(String str) {
+        return ;
+    }
+}
