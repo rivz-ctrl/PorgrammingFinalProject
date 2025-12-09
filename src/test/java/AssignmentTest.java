2b71ce01cd1a74b48bd6d066393370e0f4bdc45b
SEVEER <SEVEER221@outlook.com>
2025-12-09 18:45:29 -0500

    ran and passed 1st test

--- src/test/java/AssignmentTest.java
+++ src/test/java/AssignmentTest.java
@@ -1,2 +1,28 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.reeves.Address;
+import org.reeves.Assignment;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class AssignmentTest {
+    @Test
+    @DisplayName("[70,80,90] -> 80.0")
+    void testCalcAssignmentAverage1() {
+        ArrayList<Integer> scores = new ArrayList<>();
+        scores.add(80);
+        scores.add(90);
+        scores.add(70);
+
+        Assignment assignment = new Assignment("AssignmentTest", 25);
+        assignment.setScores(scores);
+        assignment.calcAssignmentAvg();
+
+        double expected = 80.0;
+        double actual = assignment.getAverageScore();
+        Assertions.assertEquals(expected, actual);
+    }
 }
