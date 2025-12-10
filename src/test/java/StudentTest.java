eee50745de73e406d34c51f48fd1374a63fbd2ce
SEVEER <SEVEER221@outlook.com>
2025-12-09 21:30:10 -0500

    writing testing method for student class

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,2 +1,16 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.reeves.Assignment;
+
 public class StudentTest {
+    @Test
+    @DisplayName("")
+    void testRegisterCourses1() {
+        boolean expected = true;
+        boolean actual =;
+        Assertions.assertEquals(expected, actual);
+        Assertions.assertTrue();
+
+    }
 }
