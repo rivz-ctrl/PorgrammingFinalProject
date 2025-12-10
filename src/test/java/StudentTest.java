1ad9ecb978cfeeb05e7f4515ba523b139bdab6a4
SEVEER <SEVEER221@outlook.com>
2025-12-09 21:47:40 -0500

    completed the second testing method for registerCourse

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -4,10 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.reeves.*;
 
 public class StudentTest {
-
-
     @Test
-    @DisplayName("new course -> true")
+    @DisplayName("signing up for a new course -> true")
     void testRegisterCourses1() {
         Department department = new Department("Finances");
         Address address = new Address(5555, "Av. Patrice Lumumba", "Night City" ,Address.Province.ON, "W4H4L4");
@@ -19,5 +17,21 @@ public class StudentTest {
 
         Assertions.assertEquals(expected, actual);
         Assertions.assertTrue(student.getRegisteredCourses().contains(course));
+    }
+
+    @Test
+    @DisplayName("signing up for the same course twice course -> false")
+    void testRegisterCourses2() {
+        Department department = new Department("Finances");
+        Address address = new Address(5555, "Av. Patrice Lumumba", "Night City" ,Address.Province.ON, "W4H4L4");
+        Student student = new Student("Aubrey Graham", Student.Gender.MALE, address, department);
+        Course course = new Course(department, "FF101", 2.0);
+
+        student.registerCourses(course);
+
+        boolean expected = false;
+        boolean actual = student.registerCourses(course);
+
+        Assertions.assertEquals(expected, actual);
     }
 }
