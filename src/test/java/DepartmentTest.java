9f2e6f550c3173b20bcad00c37707a56643b80e6
SEVEER <SEVEER221@outlook.com>
2025-12-09 19:39:56 -0500

    completed and passed scenario where the deparment name is invalid -> null

--- src/test/java/DepartmentTest.java
+++ src/test/java/DepartmentTest.java
@@ -6,7 +6,7 @@ import org.reeves.Department;
 public class DepartmentTest {
     @Test
     @DisplayName("Ministry Of Enjoyment -> true")
-    void testIsDepartmentNameValid() {
+    void testIsDepartmentNameValid1() {
         Department department = new Department("Ministry Of Enjoyment");
         String expectedName = "Ministry Of Enjoyment";
         String actualName = department.getDepartmentName();
@@ -16,4 +16,13 @@ public class DepartmentTest {
         String actualId = department.getDepartmentId();
         Assertions.assertEquals(expectedId, actualId);
     }
+
+    @Test
+    @DisplayName("H20 -> null")
+    void testIsDepartmentNameValid2() {
+        Department department = new Department("H2O");
+        Assertions.assertNull(department.getDepartmentName());
+        Assertions.assertNull(department.getDepartmentId());
+    }
+
 }
