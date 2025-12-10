cf6b752a3b8f7d6fec7836eb1187694abdb54c2f
SEVEER <SEVEER221@outlook.com>
2025-12-09 19:52:43 -0500

    completed testing for Department Class

--- src/test/java/DepartmentTest.java
+++ src/test/java/DepartmentTest.java
@@ -6,12 +6,59 @@ import org.reeves.Department;
 
 public class DepartmentTest {
     @Test
-    @DisplayName("Ministry of enjoyment -> true")
+    @DisplayName("MiNIstRy of ENJOYment -> true")
     void testIsDepartmentNameValid1() {
-        String str = "Ministry of enjoyment";
+        String str = "MiNIstRy of ENJOYment";
         boolean expected = true;
         boolean actual = Department.isDepartmentNameValid(str);
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    @DisplayName("H2O -> false")
+    void testIsDepartmentNameValid2() {
+        String str = "H2O";
+        boolean expected = false;
+        boolean actual = Department.isDepartmentNameValid(str);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName(" '' -> false")
+    void testIsDepartmentNameValid3() {
+        String str = "";
+        boolean expected = false;
+        boolean actual = Department.isDepartmentNameValid(str);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName(" ' ' -> false")
+    void testIsDepartmentNameValid4() {
+        String str = " ";
+        boolean expected = false;
+        boolean actual = Department.isDepartmentNameValid(str);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("H2O -> false")
+    void testIsDepartmentNameValid5() {
+        String str = "H2O";
+        boolean expected = false;
+        boolean actual = Department.isDepartmentNameValid(str);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName(" null -> false")
+    void testIsDepartmentNameValid6() {
+        String str = null;
+        boolean expected = false;
+        boolean actual = Department.isDepartmentNameValid(str);
+        Assertions.assertEquals(expected, actual);
+    }
+
+
 
 }
