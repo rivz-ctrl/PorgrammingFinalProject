2839270e165bfc66830614c9fd287731a6591143
SEVEER <SEVEER221@outlook.com>
2025-12-09 23:20:00 -0500

    completed toString test

--- src/test/java/CourseTest.java
+++ src/test/java/CourseTest.java
@@ -133,4 +133,18 @@ public class CourseTest {
         Assertions.assertArrayEquals(expected, actual);
     }
 
+    @Test
+    @DisplayName("toSimplifiedString(courseName, studentName, studentId) -> one string ")
+    void testSimplifiedString1() {
+        Department department = new Department("Finances");
+        Address address = new Address(5555, "Av. Patrice Lumumba", "Night City" ,Address.Province.ON, "W4H4L4");
+        Student student = new Student("Aubrey Graham", Student.Gender.MALE, address, department);
+        Course course = new Course(department, "FF101", 2.0);
+
+        String expected = "Finances" + " " + "Aubrey Graham" + " " + student.getStudentId() ;
+        String actual = student.toSimplifiedString();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
