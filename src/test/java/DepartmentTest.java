7706469ffa5790083d213b68f8f3e448b0608c1b
SEVEER <SEVEER221@outlook.com>
2025-12-09 19:35:06 -0500

    completed and passed first testing scenario for testIsDeparmentNameValid

--- src/test/java/DepartmentTest.java
+++ src/test/java/DepartmentTest.java
@@ -1,2 +1,19 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.reeves.Department;
+
 public class DepartmentTest {
+    @Test
+    @DisplayName("Ministry Of Enjoyment -> true")
+    void testIsDepartmentNameValid() {
+        Department department = new Department("Ministry Of Enjoyment");
+        String expectedName = "Ministry Of Enjoyment";
+        String actualName = department.getDepartmentName();
+        Assertions.assertEquals(expectedName, actualName);
+
+        String expectedId = "D01";
+        String actualId = department.getDepartmentId();
+        Assertions.assertEquals(expectedId, actualId);
+    }
 }
