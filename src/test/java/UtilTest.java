336f6a631d1bef61e373a7b36c42c78b47f290b8
SEVEER <SEVEER221@outlook.com>
2025-12-09 19:20:58 -0500

    completed toTitleCase testing methods

--- src/test/java/UtilTest.java
+++ src/test/java/UtilTest.java
@@ -22,4 +22,40 @@ public class UtilTest {
         String actual = Util.toTitleCase(str);
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    @DisplayName(" '' -> '' ")
+    void testToTitleCase3() {
+        String str = "";
+        String expected = "";
+        String actual = Util.toTitleCase(str);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("hello   world -> Hello World")
+    void testToTitleCase4() {
+        String str = "hello   world";
+        String expected = "Hello World";
+        String actual = Util.toTitleCase(str);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("hello -> Hello")
+    void testToTitleCase5() {
+        String str = "hello";
+        String expected = "Hello";
+        String actual = Util.toTitleCase(str);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("2026 ain't got nun on me, NAN! -> 2026 Ain't Got Nun On Me, Nan!")
+    void testToTitleCase6() {
+        String str = "2026 ain't got nun on me, NAN!";
+        String expected = "2026 Ain't Got Nun On Me, Nan!";
+        String actual = Util.toTitleCase(str);
+        Assertions.assertEquals(expected, actual);
+    }
 }
