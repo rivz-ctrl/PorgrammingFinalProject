091429a2b0598a040e40f58595541eda4642c0dc
SEVEER <SEVEER221@outlook.com>
2025-12-12 21:08:09 -0500

    indentation

--- src/main/java/org/reeves/Main.java
+++ src/main/java/org/reeves/Main.java
@@ -4,10 +4,13 @@ public class Main {
     public static void main(String[] args) {
 
         Department department = new Department("Finances");
-        Address address1 = new Address(5555, "Av. Patrice Lumumba", "Night City" ,Address.Province.ON, "W4H4L4");
-        Address address2 = new Address(5556, "Av. Songbird", "Dogtown" ,Address.Province.NS, "W7H7L7");
+
+        Address address1 = new Address(5555, "Av. Songbird", "Yorknew City" ,Address.Province.ON, "W4H4L4");
+        Address address2 = new Address(5556, "Av. Vagabond", "Dogtown" ,Address.Province.NS, "W7H7L7");
+
         Student student1 = new Student("Aubrey Graham", Student.Gender.MALE, address1, department);
-        Student student2 = new Student("Aubrey Graham", Student.Gender.MALE, address1, department);
+        Student student2 = new Student("Miyamoto Mushashi", Student.Gender.MALE, address2, department);
+
         Course course = new Course(department, "FF101", 2.0);
 
 
