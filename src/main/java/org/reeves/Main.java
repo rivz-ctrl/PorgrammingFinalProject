bedc9c247bc565a88ec82e93d5cd1af7963e09d1
SEVEER <SEVEER221@outlook.com>
2025-12-12 21:05:42 -0500

    writing code for aforementioned goal

--- src/main/java/org/reeves/Main.java
+++ src/main/java/org/reeves/Main.java
@@ -2,5 +2,19 @@ package org.reeves;
 
 public class Main {
     public static void main(String[] args) {
+
+        Department department = new Department("Finances");
+        Address address1 = new Address(5555, "Av. Patrice Lumumba", "Night City" ,Address.Province.ON, "W4H4L4");
+        Address address2 = new Address(5556, "Av. Songbird", "Dogtown" ,Address.Province.NS, "W7H7L7");
+        Student student1 = new Student("Aubrey Graham", Student.Gender.MALE, address1, department);
+        Student student2 = new Student("Aubrey Graham", Student.Gender.MALE, address1, department);
+        Course course = new Course(department, "FF101", 2.0);
+
+
+
+        course.addAssignment("In class exercises", 40, 100);
+        course.addAssignment("Final", 60,100);
+
+        course.registerStudent(new Student(student.getStudentName(), student.getGender(), student));
         }
 }
